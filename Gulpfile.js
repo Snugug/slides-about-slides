9e70d89bca38ef00560831172084b4669c0a050d
Sam Richard <sam@snug.ug>
2018-09-20 10:40:25 -0400

    :new: Add videos and deploy

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,7 @@ const sass = require('gulp-sass');
 const prefix = require('gulp-autoprefixer');
 const eyeglass = require('eyeglass');
 const browserSync = require('browser-sync');
+const htmlmin = require('gulp-htmlmin');
 
 const path = require('path');
 
@@ -60,7 +61,17 @@ gulp.task('html', () => {
   return gulp.src(['src/**/*.html'])
     .pipe(gulp.dest('./.www'))
     .pipe(browserSync.stream());
-})
+});
+
+gulp.task('html:build', () => {
+  return gulp.src(['src/**/*.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeComments: true,
+    }))
+    .pipe(gulp.dest('./.www'))
+});
 
 gulp.task('html:watch', ['html'], () => {
   return gulp.watch(['src/**/*.html'], ['html'])
@@ -81,11 +92,21 @@ gulp.task('videos', () => {
     .pipe(browserSync.stream());
 });
 
+/*
+ * GH Pages 
+ */
+
+
+/*
+ * Groups
+ */
 gulp.task('media:watch', ['images', 'videos'], () => {
   return gulp.watch(['src/images/**/*', 'src/videos/**/*'], ['images', 'videos']);
 });
 
 gulp.task('watch', ['js:watch', 'html:watch', 'media:watch', 'sass:watch']);
+
+gulp.task('build', ['html:build', 'images', 'videos', 'sass'])
 
 gulp.task('serve', ['watch', 'server']);
 
